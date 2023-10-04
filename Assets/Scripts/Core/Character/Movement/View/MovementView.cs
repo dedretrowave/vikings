f3c62de4ea1508a9d5d9c0d0b5bad591d3fa5588
dedretrowave <pvrvd0x-mail@yandex.ru>
2023-10-04 13:01:05 +0300

    Movement fix

--- Assets/Scripts/Core/Character/Movement/View/MovementView.cs
+++ Assets/Scripts/Core/Character/Movement/View/MovementView.cs
@@ -11,21 +11,24 @@ namespace Core.Character.Movement.View
         private const float TweenTurnSpeed = .5f;
 
         private Vector3 _direction;
-        private Vector3 _point;
+        private Vector3 _targetPoint;
         private bool _hasArrived = true;
 
         public void MoveTo(Vector3 point)
         {
-            if (_direction != Vector3.zero)
+            _targetPoint = new Vector3(point.x, transform.position.y, point.z);
+            
+            if (_direction != Vector3.zero ||
+                Vector3.Distance(transform.position, _targetPoint) <= 3f)
             {
+                _hasArrived = true;
                 return;
             }
-            
+
             _hasArrived = false;
-            _point = new(point.x, transform.position.y, point.z);
 
             transform
-                .DOMove(_point, TweenMoveSpeed)
+                .DOMove(_targetPoint, TweenMoveSpeed)
                 .onComplete = OnArrive;
 
             void OnArrive() => _hasArrived = true;
@@ -40,15 +43,15 @@ namespace Core.Character.Movement.View
         {
             if (!_hasArrived) return;
 
-            Debug.Log(_direction);
             Vector3 movement = transform.position + _direction * (_speed * Time.deltaTime);
             
             if (Vector3.Distance(transform.position, movement) <= 1f)
             {
                 return;
             }
-            
-            transform.DOMove(movement, TweenMoveSpeed);
+
+            transform.position = Vector3.MoveTowards(transform.position, movement, .3f);
+            // transform.DOMove(movement, TweenMoveSpeed);
             transform.DOLookAt(movement, TweenTurnSpeed);
         }
     }
